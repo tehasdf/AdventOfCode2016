9bc057978567ef93c1bea205b83be51ec1f2205a
asdf <tehasdf@gmail.com>
2016-12-26 20:21:24 +0100

    the "fuck readability" version for day 17

--- p17.py
+++ p17.py
@@ -1,25 +1,21 @@
+# the "fuck readability" edition
 import hashlib
 
 
-def make_next_states(state):
-    inp, (x, y) = state
+def make_next_states((inp, (x, y))):
     d = [l in 'bcdef' for l in hashlib.md5(inp).hexdigest()[:4]]
     for o, l, (xd, yd) in zip(d, 'UDLR', [(0, -1), (0, 1), (-1, 0), (1, 0)]):
         if o and (0 <= x + xd <= 3) and (0 <= y + yd <= 3):
             yield inp + l, (x + xd, y + yd)
 
 
-def _winning_states(inp):
-    states = [(inp, (0, 0))]
+def _winning_states(states):
     while states:
         for steps, pos in make_next_states(states.pop()):
-            if pos == (3, 3):
-                yield steps
-            else:
-                states.append((steps, pos))
+            (yield steps) if pos == (3, 3) else states.append((steps, pos))
 
 
 def p1(inp, part=1):
-    return {1: min, 2: max}[part](_winning_states(inp), key=len)
+    return {1: min, 2: max}[part](_winning_states([(inp, (0, 0))]), key=len)
 
 print p1('gdjjyniy', part=2)
